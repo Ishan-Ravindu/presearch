4543afdd23aa57ec684279f960615c0a24254978
Ishan-Ravindu <ishanravindu27596@gmail.com>
2023-01-22 16:42:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# presearch README
+
+## Features
+
+## Requirements
+
+## Extension Settings
+
+## Known Issues
+
+## Release Notes
+
+### 1.0.0
+
+---
+
+## Following extension guidelines
+
+## Working with Markdown
+
+## For more information
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+const URL = 'https://presearch.com/search?q=';
+
+export function activate(context: vscode.ExtensionContext) {
+	// update vscode configuration to copy the selected text from terminal
+	vscode.workspace
+		.getConfiguration()
+		.update(
+			'terminal.integrated.copyOnSelection',
+			true,
+			vscode.ConfigurationTarget.Global
+		);
+
+	let disposable = vscode.commands.registerCommand(
+		'presearch.makeSearch',
+		() => {
+			const activeTextEditor = vscode.window.activeTextEditor;
+
+			if (activeTextEditor) {
+				const selectedText = activeTextEditor.document.getText(
+					activeTextEditor.selection
+				);
+
+				vscode.env.openExternal(vscode.Uri.parse(`${URL}${selectedText}`));
+			}
+		}
+	);
+
+	let terminalDisposable = vscode.commands.registerCommand(
+		'presearch.makeSearchFromTerminal',
+		async () => {
+			const selectedText = await vscode.env.clipboard.readText();
+
+			vscode.env.openExternal(vscode.Uri.parse(`${URL}${selectedText}`));
+		}
+	);
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(terminalDisposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
